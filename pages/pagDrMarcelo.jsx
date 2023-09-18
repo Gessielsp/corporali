c2fbc46aa331bce4ed9dfb815a3741f5705e4027
Arthut Kelvim <kelvimarthur@gmail.com>
2023-09-18 20:17:27 -0300

    create: criado quase todo jsx do site

--- pages/pagDrMarcelo.jsx
+++ pages/pagDrMarcelo.jsx
@@ -1,7 +1,94 @@
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+
+import fotoGaleriaUm from "@/public/corporali/dr_marcelo/marcelo_7.JPG";
+import fotoGaleriaDois from "@/public/corporali/dr_marcelo/marcelo_6.JPG";
+import fotoGaleriaTres from "@/public/corporali/dr_marcelo/marcelo_13.JPG";
+import fotoGaleriaQuatro from "@/public/corporali/dr_marcelo/marcelo_5.JPG";
+
+import fotoCarreiraUm from "@/public/corporali/dr_marcelo/marcelo_1.jpg";
+import fotoCarreiraDois from "@/public/corporali/dr_marcelo/marcelo_2.jpg";
+import fotoCarreiraTres from "@/public/corporali/dr_marcelo/marcelo_3.jpg";
+import fotoCarreiraQuatro from "@/public/corporali/dr_marcelo/marcelo_4.jpg";
+
+import styles from "@/styles/pagDrMarcelo.module.scss";
+
+import Image from "next/image";
+
 export default function PagDrMarcelo(){
     return(
         <div>
-            Pagina Dr.Marcelo
+            <Header/>
+            <div id={styles.bannerMarcelo}>
+                <div id={styles.linhaAcimaDrMarcelo}></div>
+                <h1>Dr. Marcelo Moreira</h1>
+                <p>Bem mais que um cirurgião Plástica.</p>
+                <div id={styles.linhaAbaixoDrMarcelo}></div>
+                <div>
+                    <a href="#">Sobre Marcelo</a>
+                    <a href="#">Fotos e Videos</a>
+                </div>
+            </div>
+            <div id={styles.carroselMarcelo}></div>
+
+            <div id={styles.rotinaCirurgica}>
+                <h2>Rotina Cirurgica Intensa</h2>
+                <p>O dia a dia agitado do Dr. Marcelo Henrique começa nas primeiras horas da manhã, quando ele se prepara para uma série de cirurgias especializadas. Sua dedicação à excelência na cirurgia abdominal, facial, reparadora e mamária é evidente em cada procedimento, onde sua habilidade cirúrgica e atenção aos detalhes proporcionam resultados excepcionais aos pacientes.</p>
+            </div>
+            <div id={styles.arteDeConstruir}>
+                <h2>A Arte da Reconstrução</h2>
+                <p>Dr. Marcelo Henrique é conhecido por sua paixão em restaurar a confiança e a qualidade de vida de seus pacientes. Seu dia a dia é uma jornada dedicada à reconstrução, seja restaurando a função abdominal, a estética facial, a integridade de tecidos ou a beleza mamária. Cada cirurgia é uma oportunidade de transformar vidas.</p>
+            </div>
+            <div id={styles.cuidandoComEmpatia}>
+                <h2>Cuidando de Pacientes com Empatia</h2>
+                <p>Além de seu trabalho cirúrgico exímio, o Dr. Marcelo Henrique reserva tempo para se conectar com seus pacientes. Seu dia a dia inclui consultas pessoais, onde ele ouve atentamente as preocupações de cada indivíduo e compartilha seu conhecimento especializado. Sua abordagem empática e compassiva é uma parte essencial de sua prática médica.</p>
+            </div>
+            <div id={styles.galeriaFotosMarcelo}>
+                <div>
+                    <h1>Galeria de fotos</h1>
+                    <p>Esta galeria de fotos oferece uma visão cativante da jornada notável do Dr. Marcelo Henrique na pesquisa biomédica e suas atividades beneficentes, capturando momentos de inovação e compaixão ao longo de sua carreira inspiradora.</p>
+                </div>
+                <div>
+                    <Image src={fotoGaleriaUm} height={600} width={300} alt="Doutor Marcelo se arrumando para mais uma cirurgia"/>
+                    <Image src={fotoGaleriaDois} height={600} width={300} alt="Doutor Marcelo lavando as mãos"/>
+                    <Image src={fotoGaleriaTres} height={600} width={300} alt="Doutor Marcelo posando para foto numa sala de cirugia"/>
+                    <Image src={fotoGaleriaQuatro} height={600} width={300} alt="Doutor Marcelo conversando com sua parceira de cirurgia"/>
+                </div>
+            </div>
+            <div id={styles.carreiraMarcelo}>
+                <div>
+                    <h2>Carreira Profissional</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id molestias amet in doloremque nobis labore, soluta quod laboriosam sint at veniam iste voluptatum perspiciatis, architecto consectetur quidem quos tempora minima!</p>
+                </div>
+                <div>
+                    <div>
+                        <Image src={fotoCarreiraUm} alt="" width={300}/>
+                        <p>Dr. Marcelo Henrique começou sua jornada acadêmica com uma paixão precoce pela biomedicina. Ele se formou com distinção em Biologia Molecular na fictícia Universidade Avançada de Ciências Biomédicas. Durante seus anos de graduação, ele um talento notável para a pesquisa genética, participando de projetos.</p>
+                    </div>
+                    <div>
+                    <Image src={fotoCarreiraDois} alt="" width={300}/>
+                        <p>Dr. Marcelo Henrique começou sua jornada acadêmica com uma paixão precoce pela biomedicina. Ele se formou com distinção em Biologia Molecular na fictícia Universidade Avançada de Ciências Biomédicas. Durante seus anos de graduação, ele um talento notável para a pesquisa genética, participando de projetos.</p>
+                    </div>
+                </div>
+                <div>
+                <div>
+                        <Image src={fotoCarreiraTres} alt="" width={300}/>
+                        <p>Dr. Marcelo Henrique começou sua jornada acadêmica com uma paixão precoce pela biomedicina. Ele se formou com distinção em Biologia Molecular na fictícia Universidade Avançada de Ciências Biomédicas. Durante seus anos de graduação, ele um talento notável para a pesquisa genética, participando de projetos.</p>
+                    </div>
+                    <div>
+                    <Image src={fotoCarreiraQuatro} alt="" width={300}/>
+                        <p>Dr. Marcelo Henrique começou sua jornada acadêmica com uma paixão precoce pela biomedicina. Ele se formou com distinção em Biologia Molecular na fictícia Universidade Avançada de Ciências Biomédicas. Durante seus anos de graduação, ele um talento notável para a pesquisa genética, participando de projetos.</p>
+                    </div>
+                </div>
+            </div>
+            <div id={styles.agendeConsultaDrMarcelo}>
+                <div>
+                    <h2>Agende a sua consulta conosco</h2>
+                    <p>Transforme sua saúde hoje! Clique no botão 'Agendar Consulta' e dê o primeiro passo para uma vida melhor.</p>
+                </div>
+                <a href="#">Agendar Consulta</a>
+            </div>
+            <Footer/>
         </div>
     )
 }
--- pages/pagReparadores.jsx
+++ pages/pagReparadores.jsx
@@ -1,7 +1,81 @@
-export default function PagReparatorios(){
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+
+import styles from "@/styles/pagReparatorios.module.scss";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import imgReparadores from "@/public/corporali/procedimentos/reparadores.jpg";
+
+
+export default function PagReparadores(){
     return(
         <div>
-            Pagina reparatorios
+            <Header/>
+            <div id={styles.bannerReparadores}>
+                <div>
+                    <h1>Procedimentos Reparadores</h1>
+                    <p>Procedimentos reparadores são intervenções médicas que desempenham um papel fundamental na correção de imperfeições físicas, restaurando a função e a aparência de partes do corpo afetadas por lesões, deformidades congênitas ou condições médicas.</p>
+                </div>
+            </div>
+            <div id={styles.sobreProcedimentosReparadores}>
+                <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Cx3WX6zCFxk?si=s6BypgcP7ktP4s_v" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <div>
+                        <h2>Sobre os procedimentos</h2>
+                        <p>Os procedimentos mamários são intervenções cirúrgicas ou não-cirúrgicas que visam melhorar a estética ou corrigir problemas relacionados aos seios femininos. Isso pode incluir cirurgias de aumento, redução ou levantamento dos seios, bem como procedimentos de reconstrução após mastectomia. </p>
+                    </div>
+                </div>
+            </div>
+            <div id={styles.cancerDePele}>
+                <h2>Câncer de Pele - Tratamento</h2>
+                <p>Os preenchimentos dérmicos injetáveis se tornaram os queridinhos do mundo da estética. Tratam-se de substâncias utilizadas para dar maior volume aos lábios, melhorar contornos da face, suavizar ou eliminar rugas, podendo até serem utilizados para melhorar a aparência de cicatrizes.</p>
+            </div>
+            <div id={styles.cirurgiaIntima}>
+                <h2>Cirurgia Intima</h2>
+                <p>O excesso no tamanho dos seios causa problemas na saúde da mulher, como questões relacionadas à coluna, por exemplo – em outros casos, os problemas causados são de cunho emocional.
+                    Para resolver este problema, a mamoplastia redutora remove o excesso de gordura nos seios, bem como o tecido glandular e a pele. Este procedimento resulta em seios proporcionais ao corpo da paciente.
+                </p>
+            </div>
+            <div id={styles.correcaoCicatriz}>
+                <h2>Correção de Cicatriz</h2>
+                <p>Após acidentes ou procedimentos cirúrgicos, pessoas podem ficar com cicatrizes muito salientes. A cicatriz não pode ser completamente apagada, mas pode ser corrigida.
+                    Mesmo uma ferida que cicatriza bem pode resultar em uma cicatriz com má aparência. As opções de tratamento variam de acordo com o tipo e o grau de cicatrização e podem incluir:
+                </p>
+                <ul>
+                    <li>Tratamentos tópicos simples.</li>
+                    <li>Procedimentos minimamente invasivos.</li>
+                    <li>Revisão cirúrgica com técnicas avançadas de fechamento da ferida.</li>
+                </ul>
+                <p>A cirurgia de correção de cicatriz destina-se a minimizar a cicatriz de modo que fique mais uniforme com o seu tom de pele e a textura circundante. Apesar da correção da cicatriz proporcionar resultado estético mais agradável ou melhorar uma cicatriz que tenha má cicatrização, uma cicatriz não pode ser completamente apagada.</p>
+            </div>
+            <div id={styles.agendeConsultaReparadores}>
+                <div>
+                    <h2>Agende sua consulta conosco</h2>
+                    <p>Sua jornada para um bem-estar completo começa aqui. Agende sua consulta agora mesmo.</p>
+                </div>
+            </div>
+            <nav id={styles.linksReparadores}>
+                <ul>
+                    <li>
+                        <Link href="#">Mamarios</Link>
+                    </li>
+                    <li>
+                        <Link href="#">Faciais</Link>
+                    </li>
+                    <li>
+                        <Link href="#">Abdominais</Link>
+                    </li>
+                    <li>
+                        <Link href="#">BodyTite</Link>
+                    </li>
+                    <li>
+                        <Link href="#">Clinica</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Footer/>
         </div>
     )
 }
